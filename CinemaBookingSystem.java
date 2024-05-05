99589e3e64d85090addbc89d91ed95c9c47746ff
Fha Share <fha.share@gmail.com>
2024-05-05 12:24:13 -0400

    CInema Update

--- CinemaBookingSystem.java
+++ CinemaBookingSystem.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Write a description of class CinemaBookingSystem here.
@@ -13,6 +14,8 @@ public class CinemaBookingSystem
     //private ArrayList<Theater> theaters;
     private ArrayList<Customer> customers;
     private ArrayList<Booking> bookings;
+    
+    private List<Theater> theaters;
 
     /**
      * Constructor for objects of class CinemaBookingSystem
@@ -24,7 +27,15 @@ public class CinemaBookingSystem
         schedules = new ArrayList<>();
         schedules.add(new Schedule());
         schedules.add(new Schedule());
-        //theaters.add(new Theater());        
+        //theaters.add(new Theater()); 
+        
+        this.theaters = new ArrayList<>();
+        // Create theaters with different numbers of seats and rows
+        theaters.add(new Theater(1, 40, 5)); // Theater 1 with 40 seats, 5 rows
+        theaters.add(new Theater(2, 60, 6)); // Theater 2 with 60 seats, 6 rows
+        theaters.add(new Theater(3, 50, 5)); // Theater 3 with 50 seats, 5 rows
+        theaters.add(new Theater(4, 70, 7)); // Theater 4 with 70 seats, 7 rows
+        theaters.add(new Theater(5, 80, 8)); // Theater 5 with 80 seats, 8 rows
     }
     public void addCustomer(int phoneNumber)
     {
